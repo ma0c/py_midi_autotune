578f6b0d364d9b58a5b8c7fb227c643cc2f6c679
Mauricio Collazos <ma0@contraslash.com>
2020-02-26 10:51:24 -0500

    modified script to read interface and store file

--- record_audio.py
+++ record_audio.py
@@ -1,17 +1,31 @@
 import numpy as np
+import wave
 import sounddevice as sd
 
-sd.query_devices()
 
-sd.default.device = 6
+def record_audio():
+    print(sd.query_devices())
 
-sample_rate = 16000
-channels = 1
+    selected_device = int(input("Select the device: "))
 
-recording = sd.rec(sample_rate*3, samplerate=sample_rate, channels=1)
-sd.wait()
+    sd.default.device = selected_device
 
-print(np.max(recording))
-print(recording)
+    sample_rate = 16000
+    channels = 1
 
-sd.play(recording, sample_rate)
+    recording = sd.rec(sample_rate*2, samplerate=sample_rate, channels=channels)
+    sd.wait()
+
+    print(recording.shape)
+
+    # sd.play(recording, sample_rate)
+    AH_440 = wave.open("AH_440.wav", "wb")
+    AH_440.setnchannels(channels)
+    AH_440.setsampwidth(4)
+    AH_440.setframerate(sample_rate)
+    AH_440.writeframes(recording)
+    AH_440.close()
+
+
+if __name__ == '__main__':
+    record_audio()
